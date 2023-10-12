feb881b6b6959a8a5e5f91cef373c62313bbce33
Achieve-software <vtcbcdtyr@gmail.com>
2023-10-13 01:22:43 +0300

    index

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
@@ -8,24 +8,52 @@
     $ npm i jsonwebtoken
 */
 
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 /* ------------------------------------------------------- */
+//Required
+require("dotenv").config();
+const PORT = process.env?.PORT || 8000;
 
-
-
+require("express-async-errors");
 // continue from here...
 
+//config
 
-
+//connect to db
+const { dbConnection } = require("./src/configs/dbConnection");
+dbConnection();
 /* ------------------------------------------------------- */
 
+//middlewares
+
+//accept json
+app.use(express.json());
+
+// SessionsCookies:
+app.use(require("cookie-session")({ secret: process.env.SECRET_KEY }));
+
+// res.getModelList():
+app.use(require("./src/middlewares/findSearchSortPage"));
+
+// Routes:
+
+// HomePath:
+app.all("/", (req, res) => {
+  res.send({
+    error: false,
+    message: "Welcome to PERSONNEL API",
+    session: req.session,
+    isLogin: req.isLogin,
+  });
+});
+
 // errorHandler:
-app.use(require('./src/middlewares/errorHandler'))
+app.use(require("./src/middlewares/errorHandler"));
 
 // RUN SERVER:
-app.listen(PORT, () => console.log('http://127.0.0.1:' + PORT))
+app.listen(PORT, () => console.log("http://127.0.0.1:" + PORT));
 
 /* ------------------------------------------------------- */
 // Syncronization (must be in commentLine):
